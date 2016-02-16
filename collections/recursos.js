121d4ba4e442eb176e14270497a17ded69569022
Santiago Melo <smeloa@yahoo.com>
2016-02-16 08:55:29 -0500

    Recursos Collection Schema Defined

--- collections/recursos.js
+++ collections/recursos.js
@@ -1,1 +1,61 @@
 Recursos = new Mongo.Collection('recursos');
+
+RecursosIndex = new EasySearch.Index({
+  collection: Recursos,
+  fields: ['nombre'],
+  engine: new EasySearch.MongoDB({
+    sort: () => ['nonmbre']
+  })
+});
+
+if (Meteor.isServer) {
+  Recursos.allow({
+    insert: function(userId, doc) {
+      return !!userId;
+    },
+    update: function(userId, doc) {
+      return !!userId;
+    }
+  });
+}
+
+RecursosSchema = new SimpleSchema({
+  nombre: {
+    type: String,
+    label: "Nombre"
+  },
+  descripcion: {
+    type: String,
+    label: "Descripcion"
+  },
+  tags: {
+    type: String,
+    label: "Tipo",
+    autoform: {
+      options: [
+        {"label": "Actividades", "value": "Actividades"},
+        {"label": "Colegios", "value": "Colegios"},
+        {"label": "Jardines", "value": "Jardines"},
+      ],
+      afField: {
+        firstOption: "(Selecciona un tipo)"
+      }
+    }
+  },
+  url: {
+    type: String,
+    label: "URL"
+  },
+  createdAt: {
+    type: Date,
+    autoValue: function() {
+      return new Date();
+    }
+  },
+  author: {
+    type: String,
+    autoValue: function() {
+      return this.userId;
+    }
+  }
+});

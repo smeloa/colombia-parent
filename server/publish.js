7595af04634d54215b724890adb9fba09b41a4fb
Santiago Melo <smeloa@yahoo.com>
2016-05-13 12:20:04 -0400

    Fixed bug with resource card map not pointing to right place

--- server/publish.js
+++ server/publish.js
@@ -1,8 +1,3 @@
 Meteor.publish('recursos', function() {
   return Recursos.find({});
 });
-//
-// Meteor.publish('recursos', function() {
-//   let today = new Date();
-//   return Recursos.find({ 'recursos.fechaEnd': { $gt: today }});
-// });

50e944cd41fdf9e0678f8c383ee51332b014c40b
Luis Matheus Paris <ing.luis.matheus@outlook.com>
2016-05-14 13:26:08 -0500

    fixed users publication

--- server/publish.js
+++ server/publish.js
@@ -2,6 +2,6 @@ Meteor.publish('recursos', function() {
   return Recursos.find({});
 });
 
-Meteor.publish("users", function(argument){
-  return Meteor.users({},{fields:{emails:1}})
+Meteor.publish("users", function(){
+  return Meteor.users.find({},{fields:{emails:1}})
 });

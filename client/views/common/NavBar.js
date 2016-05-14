f22f65ffe471c97772b20b5fbc6f54731cca54d5
Luis Matheus Paris <ing.luis.matheus@outlook.com>
2016-05-13 23:58:01 -0500

    added opacity depending on height

--- client/views/common/NavBar.js
+++ client/views/common/NavBar.js
@@ -1,3 +1,14 @@
+Template.NavBar.onRendered(function(){
+  var initialHeight = $(".jumbotron").height() + 40;
+  $navbar = $('.navbar');
+   $(document).scroll(function() {
+     var opacity = $(this).scrollTop() / initialHeight;
+       $navbar.css({
+           "background": "rgba(0,0,0," + opacity + ")"
+       });
+   });
+});
+
 Template.NavBar.events({
   'click .login-toggle': ()=> {
     Session.set('nav-toggle', 'open');
